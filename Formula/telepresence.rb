d57b7db1200d445da78cbee7ec993d90b7658257
Mark Nefedov <contact@marknefedov.dev>
2022-10-22 20:16:04 +0000

    telepresence: add 2.8.2 bottle.

--- Formula/telepresence.rb
+++ Formula/telepresence.rb
@@ -11,6 +11,12 @@ class Telepresence < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/markusgod/homebrew-tools/releases/download/telepresence-2.8.2"
+    sha256 cellar: :any_skip_relocation, monterey:     "3de1467de95f0ea7a0b5ec43bf1e26e0ededfd19f09dcad78014faa00f2f080c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "883cc3d8d546d716546dd99d8d380a06692b0cd0df0edc01984c1ec97cd0e013"
+  end
+
   depends_on "go" => :build
 
   def install
