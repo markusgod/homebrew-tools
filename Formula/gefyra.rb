ee224514186fcd187dc70e0ac10f9568914c51dc
Mark Nefedov <contact@marknefedov.dev>
2022-10-22 23:54:50 +0300

    Code styling

--- Formula/gefyra.rb
+++ Formula/gefyra.rb
@@ -2,20 +2,20 @@ class Gefyra < Formula
   desc "Blazingly-fast, rock-solid, local application development with Kubernetes"
   homepage "https://gefyra.dev"
   url "https://github.com/gefyrahq/gefyra.git",
-      tag:      "v0.11.0"      
+      tag: "v0.11.0"      
   license "Apache-2.0"
 
   livecheck do
     url :stable
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
-  
+
   depends_on "pyoxidizer" => :build
-  
+
   def install
     system "pyoxidizer", "build",  "exe", "--release"
     system "cp `find build/ -name \"gefyra\"` ../gefyra"
-    system "strip -s ../gefyra"
+    system "strip" "-s" "../gefyra"
     bin.install "../gefyra"
   end
 
