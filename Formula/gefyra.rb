9ab4813bb19c133ba0fbfba29e2d40f207832dd2
Mark Nefedov <contact@marknefedov.dev>
2022-10-23 00:28:38 +0300

    checksum

--- Formula/gefyra.rb
+++ Formula/gefyra.rb
@@ -2,6 +2,7 @@ class Gefyra < Formula
   desc "Blazingly-fast, rock-solid, local application development with Kubernetes"
   homepage "https://gefyra.dev"
   url "https://github.com/gefyrahq/gefyra/archive/refs/tags/0.11.4.tar.gz"
+  sha256 "e89f99b78c08647f1bee267b8712ef3bfe5f999cc98db339fadd0aeb26d6ed98"
   license "Apache-2.0"
   revision 1
   head "https://github.com/gefyrahq/gefyra.git", branch: "master"
