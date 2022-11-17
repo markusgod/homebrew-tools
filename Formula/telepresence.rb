ba7f9340761619fb5f7ac7514167fbfdc0930009
Mark Nefedov <contact@marknefedov.dev>
2022-11-17 05:32:49 +0000

    telepresence: update 2.9.2 bottle.

--- Formula/telepresence.rb
+++ Formula/telepresence.rb
@@ -12,9 +12,9 @@ class Telepresence < Formula
   end
 
   bottle do
-    root_url "https://github.com/markusgod/homebrew-tools/releases/download/telepresence-2.9.1"
-    sha256 cellar: :any_skip_relocation, monterey:     "dc27484c9241ef0dfd65e72d265e4c4283b8f875ddcaf0c8cae04f06f032dceb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "576991cba00fcd67ea65950a205d467673e62a40a5e2247cbb09a68448c8a306"
+    root_url "https://github.com/markusgod/homebrew-tools/releases/download/telepresence-2.9.2"
+    sha256 cellar: :any_skip_relocation, monterey:     "5898b5d376ea9f5c61c51946d13f5032f53bdcd0fe02eb351989cfb3ba1e280a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "33cbbd7bf14625ecf975f70525ae4f9f40de225e9206d7ecca74fac2905cd985"
   end
 
   depends_on "go" => :build
