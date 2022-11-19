5d7571b20a2c602a552865181ff9783d5bd56b8d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-11-19 05:17:47 +0000

    gefyra 0.13.3

--- Formula/gefyra.rb
+++ Formula/gefyra.rb
@@ -1,8 +1,8 @@
 class Gefyra < Formula
   desc "Blazingly-fast, rock-solid, local application development with Kubernetes"
   homepage "https://gefyra.dev"
-  url "https://github.com/gefyrahq/gefyra/archive/refs/tags/0.13.2.tar.gz"
-  sha256 "7d19df01af64504f578bd274952d68b5aecfdfa21b8a4f569c600f357e02db09"
+  url "https://github.com/gefyrahq/gefyra/archive/refs/tags/0.13.3.tar.gz"
+  sha256 "b8e6df01f8450979b3ed82c1df86a9f678a044eb4c70089cdd6efb3103f5973a"
   license "Apache-2.0"
   head "https://github.com/gefyrahq/gefyra.git", branch: "main"
 
